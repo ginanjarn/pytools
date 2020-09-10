7955445ef21b00737b7ba1f860949922d229a047
ginanjarn <ginanjarn@gmail.com>
2020-09-10 22:08:59 +0700

    change port

--- langserver/client/service.py
+++ langserver/client/service.py
@@ -118,7 +118,7 @@ class Client:
             if occured, or None"""
         try:
             HOST = '127.0.0.1'  # The server's hostname or IP address
-            PORT = 65432        # The port used by the server
+            PORT = 9364        # The port used by the server
 
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 s.connect((HOST, PORT))
--- langserver/server/main.py
+++ langserver/server/main.py
@@ -83,9 +83,10 @@ class Server:
     def run_service(self):
         HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
         # Port to listen on (non-privileged ports are > 1023)
-        PORT = 65432
+        PORT = 9364
 
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR, 1)
             s.bind((HOST, PORT))
             s.listen()
             while True:
