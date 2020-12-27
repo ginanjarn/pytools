44cf60926ea6314c6f147d485cf2c2d3f2b7952a
ginanjarn <ginanjarn@gmail.com>
2020-12-27 18:47:11 +0700

    format lint module

--- lint.py
+++ lint.py
@@ -7,14 +7,12 @@ import threading
 import sublime  # pylint: disable=import-error
 import sublime_plugin  # pylint: disable=import-error
 from . import diagnostic
-# import diagnostic
 
 
 logger = logging.getLogger(__name__)
 # logger.setLevel(logging.DEBUG)
 sh = logging.StreamHandler()
-sh.setFormatter(logging.Formatter(
-    '%(levelname)s\t%(module)s: %(lineno)d\t%(message)s'))
+sh.setFormatter(logging.Formatter("%(levelname)s\t%(module)s: %(lineno)d\t%(message)s"))
 sh.setLevel(logging.DEBUG)
 logger.addHandler(sh)
 
@@ -27,7 +25,7 @@ def load_settings(key):
 def get_sysenv():
     new_paths = load_settings("path")
     env = os.environ.copy()
-    env['PATH'] = new_paths + os.path.pathsep + env['PATH']
+    env["PATH"] = new_paths + os.path.pathsep + env["PATH"]
     return env
 
 
@@ -58,9 +56,17 @@ class Marker:
 
         flag = {
             ERROR: sublime.DRAW_NO_OUTLINE,
-            WARNING: sublime.DRAW_NO_FILL | sublime.DRAW_NO_OUTLINE | sublime.DRAW_SOLID_UNDERLINE,
-            INFORMATION: sublime.DRAW_NO_FILL | sublime.DRAW_NO_OUTLINE | sublime.DRAW_SQUIGGLY_UNDERLINE | sublime.HIDE_ON_MINIMAP,
-            HINT: sublime.DRAW_NO_FILL | sublime.DRAW_NO_OUTLINE | sublime.DRAW_SQUIGGLY_UNDERLINE | sublime.HIDE_ON_MINIMAP,
+            WARNING: sublime.DRAW_NO_FILL
+            | sublime.DRAW_NO_OUTLINE
+            | sublime.DRAW_SOLID_UNDERLINE,
+            INFORMATION: sublime.DRAW_NO_FILL
+            | sublime.DRAW_NO_OUTLINE
+            | sublime.DRAW_SQUIGGLY_UNDERLINE
+            | sublime.HIDE_ON_MINIMAP,
+            HINT: sublime.DRAW_NO_FILL
+            | sublime.DRAW_NO_OUTLINE
+            | sublime.DRAW_SQUIGGLY_UNDERLINE
+            | sublime.HIDE_ON_MINIMAP,
         }
         return flag[severity]
 
@@ -68,12 +74,7 @@ class Marker:
     def get_icon(severity):
         """get icon follow severity"""
 
-        icon = {
-            ERROR: "circle",
-            WARNING: "dot",
-            INFORMATION: "dot",
-            HINT: "bookmark"
-        }
+        icon = {ERROR: "circle", WARNING: "dot", INFORMATION: "dot", HINT: "bookmark"}
         return icon[severity]
 
     def __init__(self):
@@ -101,20 +102,27 @@ class Marker:
             **kwargs: additional mapped data
         """
 
-        line -= 1       # sublime use zero(0) based line index
+        line -= 1  # sublime use zero(0) based line index
         if self.is_prioritized(file_name, line, severity):
             key = Marker.mark_key % (file_name, line)
             self.marks[key] = {
-                "file_name": file_name, "line": line, "column": column,
-                "severity": severity, "message": message
+                "file_name": file_name,
+                "line": line,
+                "column": column,
+                "severity": severity,
+                "message": message,
             }
             self.marks[key].update(kwargs)
 
     def add_regions(self, view, scope, regions, severity):
         key = "%s%s" % (view.file_name(), severity)
-        view.add_regions(key=key, regions=regions, scope=scope,
-                         icon=Marker.get_icon(severity),
-                         flags=Marker.get_flag(severity))
+        view.add_regions(
+            key=key,
+            regions=regions,
+            scope=scope,
+            icon=Marker.get_icon(severity),
+            flags=Marker.get_flag(severity),
+        )
 
     def mark_error(self, view, regions):
         self.add_regions(view, "invalid", regions, ERROR)
@@ -135,32 +143,39 @@ class Marker:
         err_regs, warn_regs, info_regs, hint_regs = [], [], [], []
 
         def match(marks, file_name, severity):
-            return (marks["file_name"] == file_name) and (
-                marks["severity"] == severity)
+            return (marks["file_name"] == file_name) and (marks["severity"] == severity)
 
         file_name = view.file_name()
-        messages = filter(lambda mark: match(
-            mark, file_name, ERROR), self.marks.values())
-        err_regs = [self.get_region(view, msg["line"], msg["column"])
-                    for msg in messages]
+        messages = filter(
+            lambda mark: match(mark, file_name, ERROR), self.marks.values()
+        )
+        err_regs = [
+            self.get_region(view, msg["line"], msg["column"]) for msg in messages
+        ]
         self.mark_error(view, err_regs)
 
-        messages = filter(lambda mark: match(
-            mark, file_name, WARNING), self.marks.values())
-        warn_regs = [self.get_region(view, msg["line"], msg["column"])
-                     for msg in messages]
+        messages = filter(
+            lambda mark: match(mark, file_name, WARNING), self.marks.values()
+        )
+        warn_regs = [
+            self.get_region(view, msg["line"], msg["column"]) for msg in messages
+        ]
         self.mark_warning(view, warn_regs)
 
-        messages = filter(lambda mark: match(
-            mark, file_name, INFORMATION), self.marks.values())
-        info_regs = [self.get_region(view, msg["line"], msg["column"])
-                     for msg in messages]
+        messages = filter(
+            lambda mark: match(mark, file_name, INFORMATION), self.marks.values()
+        )
+        info_regs = [
+            self.get_region(view, msg["line"], msg["column"]) for msg in messages
+        ]
         self.mark_information(view, info_regs)
 
-        messages = filter(lambda mark: match(
-            mark, file_name, HINT), self.marks.values())
-        hint_regs = [self.get_region(view, msg["line"], msg["column"])
-                     for msg in messages]
+        messages = filter(
+            lambda mark: match(mark, file_name, HINT), self.marks.values()
+        )
+        hint_regs = [
+            self.get_region(view, msg["line"], msg["column"]) for msg in messages
+        ]
         self.mark_hint(view, hint_regs)
 
     def get_message(self, file_name, line):
@@ -176,8 +191,9 @@ class Marker:
     def clear(self, view, severity="all"):
 
         file_name = view.file_name()
-        severity = [ERROR, WARNING, INFORMATION,
-                    HINT] if severity == "all" else severity
+        severity = (
+            [ERROR, WARNING, INFORMATION, HINT] if severity == "all" else severity
+        )
 
         def make_key(svt):
             return "%s%s" % (file_name, svt)
@@ -190,8 +206,9 @@ class Marker:
         def match(value, file_name):
             return value["file_name"] == file_name
 
-        key_to_remove = [key for key, value in self.marks.items()
-                         if match(value, file_name)]
+        key_to_remove = [
+            key for key, value in self.marks.items() if match(value, file_name)
+        ]
 
         for key in key_to_remove:
             del self.marks[key]
@@ -216,12 +233,17 @@ class PytoolsLintCommand(sublime_plugin.TextCommand):
 
         lint = diagnostic.Pylint(file_path, env=env)
         result_msg = lint.lint(template=diagnostic.PylintFormatter.template)
-        formatted_messages = diagnostic.PylintFormatter.parse_output(
-            result_msg)
+        formatted_messages = diagnostic.PylintFormatter.parse_output(result_msg)
 
         for msg in formatted_messages:
-            MARKER.add_mark(file_name, msg["line"], msg["column"],
-                            msg["severity"], msg["message"], msg_code=msg["code"])
+            MARKER.add_mark(
+                file_name,
+                msg["line"],
+                msg["column"],
+                msg["severity"],
+                msg["message"],
+                msg_code=msg["code"],
+            )
 
         MARKER.apply(view)
 
@@ -243,8 +265,13 @@ class Linter(sublime_plugin.EventListener):
 
     def show_popup(self, view, content, location):
         if content is not None:
-            view.show_popup(content, sublime.HIDE_ON_MOUSE_MOVE_AWAY,
-                            location=location, max_width=900, on_navigate=None)
+            view.show_popup(
+                content,
+                sublime.HIDE_ON_MOUSE_MOVE_AWAY,
+                location=location,
+                max_width=900,
+                on_navigate=None,
+            )
 
     def on_post_save_async(self, view):
         MARKER.clear(view)
