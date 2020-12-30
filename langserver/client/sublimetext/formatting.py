9dfb086cc64d4a4ccd3faf610545e818da3f76bb
ginanjarn <ginanjarn@gmail.com>
2020-12-30 09:24:30 +0700

    revert client..formatting module

--- langserver/client/sublimetext/formatting.py
+++ langserver/client/sublimetext/formatting.py
@@ -1,36 +1,61 @@
-import logging
 import sublime
+import logging
 
 logger = logging.getLogger(__name__)
 # logger.setLevel(logging.DEBUG)
 sh = logging.StreamHandler()
-sh.setFormatter(logging.Formatter("%(levelname)s\t%(module)s: %(lineno)d\t%(message)s"))
+sh.setFormatter(logging.Formatter('%(levelname)s\t%(module)s: %(lineno)d\t%(message)s'))
 sh.setLevel(logging.DEBUG)
 logger.addHandler(sh)
 
 
 def update_edit(view, edit, new_values):
     try:
+        if not new_values:
+            return
         src = view.substr(sublime.Region(0, view.size()))
-        newsrc = apply_update(src, new_values)
+        newsrc = applyUpdate(src, new_values)
         view.erase(edit, sublime.Region(0, view.size()))
         view.insert(edit, 0, newsrc)
-    except ValueError:
+    except Exception:
         logger.exception("update_edit", exc_info=True)
 
 
-def apply_update(src, updates):
+def applyUpdate(src, update):
+    results = None
     try:
-        srcs = src.split("\n")
-        for text_edit in updates:
-            start = int(text_edit["range"]["start"]["line"]) - 1
-            end = int(text_edit["range"]["end"]["line"])
-            del srcs[start:end]
-            srcs.insert(start, text_edit["newText"])
-        results = "\n".join(srcs)
-    except Exception:
-        logger.exception("error applying updates", exc_info=True)
-        raise ValueError("unable to extract") from None
+        if not update:
+            return
+        lines = src.split("\n")
+        lines = (l for l in lines)
+        line_i, update_i = 0, 0
+        new_src = []
+        pass_insert = False
+        len_update = len(update)
+        logger.debug(src)
+        logger.debug(update)
 
-    logger.debug(results)
-    return results
+        for line in lines:
+            line_i += 1
+            if update_i >= len_update:
+                pass_insert = False
+            else:
+                if line_i == update[update_i]["range"]['start']["line"]:
+                    pass_insert = True
+                    new_src.append(update[update_i]["newText"])
+                    continue
+                elif line_i == update[update_i]["range"]['end']["line"]:
+                    pass_insert = False
+                    update_i += 1
+                    continue
+                elif update[update_i]["range"]['start']["line"] == update[update_i]["range"]['end']["line"]:
+                    pass_insert = False
+                    update_i += 1
+            if not pass_insert:
+                new_src.append(line)
+        results = "\n".join(new_src)
+        logger.debug(results)
+    except Exception:
+        logger.exception("applyUpdate", exc_info=True)
+    finally:
+        return results
